d8310863ba937972e8a487cb40ad749ac68f206b
Adesh Kumar Patra <adeshkpatra@akpsmac-3.local>
2024-04-21 17:25:55 +0530

    Prettify UI

--- packages/ui/src/card.jsx
+++ packages/ui/src/card.jsx
@@ -3,9 +3,9 @@ import ColorInput from "./colorInput"
 
 const Card = ({ setColor }) => {
   return (
-    <div className="mt-3 w-11/12 rounded-2xl border-2 border-black shadow-none px-2 py-1 text-background">
+    <div className="mt-3 w-11/12 rounded-2xl border-2 border-black shadow-none px-2 pt-2 text-background">
       <div className="text-xs sm:text-base text-left">CHOOSE INITIAL COLOR</div>
-      <div className="py-1.5 grid grid-cols-4">
+      <div className="pt-3 sm:pt-2 grid grid-cols-4">
         <ColorInput setColor={setColor} type="red" />
         <ColorInput setColor={setColor} type="green" />
         <ColorInput setColor={setColor} type="blue" />
