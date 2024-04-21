cac5987e7651a2c6703f8dad6288364d261df8a9
Adesh Kumar Patra <adeshkpatra@akpsmac-3.local>
2024-04-21 17:11:56 +0530

    Responsiveness bg

--- packages/ui/src/card.jsx
+++ packages/ui/src/card.jsx
@@ -3,7 +3,7 @@ import ColorInput from "./colorInput"
 
 const Card = ({ setColor }) => {
   return (
-    <div className="mt-3 w-10/12 rounded-2xl border-2 border-black shadow-none pl-2 pr-2 py-1 text-background">
+    <div className="mt-3 w-10/12 rounded-2xl border-2 border-black shadow-none px-1 sm:px-2 py-1 text-background">
       <div className="text-xs sm:text-base text-left">CHOOSE INITIAL COLOR</div>
       <div className="py-1.5 grid grid-cols-4">
         <ColorInput setColor={setColor} type="red" />
